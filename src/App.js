f474828abbf638954666a2af31fdfcae8b05a7fa
Lanza <lanzathm@gmail.com>
2020-07-27 18:29:53 -0300

    :sparkles: Components added to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
 import React from 'react';
 import Menu from './components/Menu';
+import BannerMain from './components/BannerMain';
+import Carousel from './components/Carousel';
+import Footer from './components/Footer';
+import mainData from './data/main_data.json';
 
 function App() {
   return (
-    <div>
+    <div style={{ background: "#131313" }}>
         <Menu />
+        <BannerMain
+            videoTitle={mainData.categories[0].videos[3].title}
+            url={mainData.categories[0].videos[3].url}
+        />
+
+        <Carousel
+            ignoreFirstVideo
+            category={mainData.categories[0]}
+        />
+
+        <Carousel
+            category={mainData.categories[1]}
+        />
+
+        <Carousel
+            category={mainData.categories[2]}
+        />
+
+        <Carousel
+            category={mainData.categories[3]}
+        />
+
+        <Carousel
+            category={mainData.categories[4]}
+        />
+
+        <Carousel
+            category={mainData.categories[5]}
+        />
+
+        <Footer />
     </div>
   );
 }
