5914f3d87e12273b76fd86d7a6005aa19bb154ee
Lanza <lanzathm@gmail.com>
2020-07-28 21:47:56 -0300

    :lipstick: New styles added

---
+++ src/components/Layout/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Main = styled.main`
+    background-color: var(--secondary);
+    color: var(--white);
+    flex: 1;
+    @media (max-width: 800px) {
+        padding-left: 1%;
+        padding-right: 1%;
+    }
+`;
--- src/pages/404/styles.js
+++ src/pages/404/styles.js
@@ -1,23 +1,29 @@
 import styled from 'styled-components';
 
 export const MessageContainer = styled.div`
-    background: var(--black);
+    background: var(--secondary);
     display: flex;
     flex-wrap: wrap;
     flex-direction: column;
     align-items: center;
-    font-size: 40px;
     min-height: 500px;
     color: var(--white);
     text-align: center;
+    h2 {
+        font-size: 36px;
+        padding-bottom: 32px;
+        line-height: 16px
+    }
     @media (min-width: 1400px) {
         min-height: 700px;
     }
-    @media (max-width: 900px) {
+    @media (max-width: 800px) {
     font-size: 20px;
+    padding: 0;
     img {
         border-radius: 30px;
         max-width: 120px;
+        padding-top: 120px;
         height: auto;
     }
   }
