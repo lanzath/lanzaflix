11bb68cd186d01efb70db0bbe5a995b48622a4d9
Lanza <lanzathm@gmail.com>
2020-07-28 21:57:59 -0300

    :pencil2: Updated 404 page

--- src/pages/404/index.js
+++ src/pages/404/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { MessageContainer, ImgMeme } from './styles';
 import CatImage from '../../assets/img/notFoundmeme.png'
-import Menu from '../../components/Menu';
-import Footer from '../../components/Footer';
+import Layout from '../../components/Layout';
 
 /**
  * Render a 404 - Not found page
@@ -10,14 +9,12 @@ import Footer from '../../components/Footer';
  */
 function NotFound() {
     return (
-        <>
-            <Menu />
+        <Layout>
             <MessageContainer>
-                <h1>404 - Not Found</h1>
+                <h2>404 - Not Found</h2>
                 <ImgMeme src={CatImage}/>
             </MessageContainer>
-            <Footer />
-        </>
+        </Layout>
     );
 }
 
