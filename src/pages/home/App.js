4c2a798b797b36103325561c3a025fc6ab327d59
Lanza <lanzathm@gmail.com>
2020-07-31 19:23:19 -0300

    :sparkles: Home rendering Carousel dynamically

--- src/pages/home/App.js
+++ src/pages/home/App.js
@@ -1,45 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import BannerMain from '../../components/BannerMain';
 import Carousel from '../../components/Carousel';
-import mainData from '../../data/main_data.json';
 import Layout from '../../components/Layout';
+import categoriesRepository from '../../repositories/categories';
 
 /**
  * Main App component, home page
  * @returns {Element} JSX
  */
 function Home() {
+  const [mainData, setMainData] = useState([]);
+
+  useEffect(() => {
+    categoriesRepository.getAllWithVideos()
+      .then(categoriesWithVideos => setMainData(categoriesWithVideos));
+  }, []);
+
+  //http://localhost:8080/categories?_embed=videos
+
   return (
     <Layout>
-      <BannerMain
-        videoTitle={mainData.categories[2].videos[0].title}
-        url={mainData.categories[2].videos[0].url}
-      />
+      {mainData.length === 0 && (<div>Loading...</div>)}
 
-      <Carousel
-        category={mainData.categories[0]}
-      />
+      {mainData.map((category, index) => {
+        if (index === 0) {
+          return (
+            <div key={category.id}>
+              <BannerMain
+                videoTitle={mainData[0].videos[0].title}
+                url={mainData[0].videos[0].url}
+              />
+              <Carousel
+                category={mainData[0]}
+              />
+            </div>
+          );
+        }
 
-      <Carousel
-        category={mainData.categories[1]}
-      />
-
-      <Carousel
-        ignoreFirstVideo
-        category={mainData.categories[2]}
-      />
-
-      <Carousel
-        category={mainData.categories[3]}
-      />
-
-      <Carousel
-        category={mainData.categories[4]}
-      />
-
-      <Carousel
-        category={mainData.categories[5]}
-      />
+        return (
+          <Carousel
+            key={category.id}
+            category={category}
+          />
+        );
+      })}
     </Layout>
   );
 }
